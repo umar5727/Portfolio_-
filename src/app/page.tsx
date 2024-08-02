5f0560d23e8903cb5a6d4666a58bc55e3b3ff6f3
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-08-02 23:57:44 +0530

    setup testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Basic Setup
+      <section className="grid md:grid-cols-2 gap-10">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl">Hy! I am Umar Khan</h1>
+          <h3 className="text-xl">A Full Stack Developer</h3>
+        </div>
+        <div>
+
+        </div>
+      </section>
     </main>
   );
 }
