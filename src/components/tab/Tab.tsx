4939c02043f9fda0d266f2d898295d7c4dc6a9ab
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-08-12 23:30:58 +0530

    working on ui

---
+++ src/components/tab/Tab.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tab() {
+    return (
+        <section>
+            <div>heading</div>
+            <div>content</div>
+        </section>
+    )
+}
