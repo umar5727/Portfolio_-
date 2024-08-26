49d4e7c991d77720731a3f9167940f8febba6594
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-08-26 18:58:54 +0530

    used next.js Image tag and layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full flex flex-col items-center justify-between px-24 text-primarywhite">
-      <Header />
-      <section className="relative w-full h-screen grid md:grid-cols-2 gap-10 justify-center items-center pt-12 mb-10">
+      
+      <section className="relative w-full h-[calc(100vh-48px)] grid md:grid-cols-2 gap-10 justify-center items-center mb-10">
         <motion.div 
           className="flex flex-col gap-2 justify-center"
           variants={{
@@ -18,11 +18,22 @@ export default function Home() {
             }}
             initial='hidden'
             animate='visible'
-            transition={{duration:0.5, delay:0.25}}
+            transition={{duration:0.25}}
         >
-          <h1 className="text-6xl">Hy! I'm <span className="text-primary">Umar</span></h1>
-          <h3 className="text-xl">A Full Stack Developer</h3>
-          <p>I use  MERN Stack to Build Modern development</p>
+          <motion.div 
+            className="flex flex-col gap-2 justify-center overflow-hidden"
+            variants={{
+              hidden:{y:75,opacity:0},
+              visible:{y:0, opacity:1}
+            }}
+            initial='hidden'
+            animate='visible'
+            transition={{duration:.5,delay:.5}}
+          >
+            <h1 className="text-6xl">Hy! I'm <span className="text-primary">Umar</span></h1>
+            <h3 className="text-xl">A Full Stack Developer</h3>
+            <p>I use  MERN Stack to Build Modern development</p>
+          </motion.div>
           <div className="flex flex-col gap-2 items-start mt-2">
             <button className="px-4 py-1 bg-purple-800 rounded-md">About Me</button>
             <button className="px-4 py-1 bg-purple-800 rounded-md">Resume</button>
@@ -30,8 +41,9 @@ export default function Home() {
         </motion.div>
         {/* left ends  */}
         <div className="flex justify-center items-center aspect-square">
-          <div className="w-full bg-purple-600 aspect-square flex justify-center items-center rounded-full overflow-hidden ">
-            <img src="self-without-bg.png" alt="profile image" />
+          <div className="relative w-full bg-purple-600 aspect-square flex justify-center items-center rounded-full overflow-hidden ">
+            <Image src="/self-without-bg.png"  alt="profile image" fill className="!w-3/4 !left-[12%] !right-[12%]"/>
+            {/* <img src="/self-without-bg.png"  alt="profile image" /> */}
           </div>
         </div>
         <div className="absolute bottom-4 left-1/2 -translate-x-1/2">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className='absolute top-0 left-0 w-full px-24 h-12 bg-red-400'>
-            <div className='flex justify-between '>
+        <header className='w-full  px-24 h-12 bg-red-400 flex items-center'>
+            <div className='w-full flex justify-between items-center'>
                 <div>logo</div>
                 <div>Intro </div>
             </div>
