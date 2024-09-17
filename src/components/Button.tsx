09710a2a90c59f3aded1e38ba9fb23ccfa1eb219
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-09-17 22:01:27 +0530

    working on skills section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,6 @@ export default function Button({children, className,a}:buttoninterface) {
   return (
     <button className={`px-4 py-1 bg-purple-800 rounded-md text-white font-semibold duration-300  hover:text-purple-800 hover:bg-white ${className}`}>
       {children}
-    </button>
+    </button >
   )
 }
